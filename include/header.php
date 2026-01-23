ed4d73682bcffa743070d33146ddbc72afe36911
VOGLOSSOU <n969601@gmail.com>
2026-01-23 14:49:31 +0100

    Replace HTML pages with PHP implementation

---
+++ include/header.php
@@ -0,0 +1,270 @@
+
+   
+   <!--[if lte IE 9]>
+    	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  	<![endif]-->
+
+
+    <!--********************************
+    		Code Start From Here 
+	******************************** -->
+    <div class="magic-cursor relative z-10">
+        <div class="cursor"></div>
+        <div class="cursor-follower"></div>
+    </div>
+
+    <div class="cursor-follower"></div>
+
+    <!-- slider drag cursor -->
+    <div class="slider-drag-cursor"><img src="assets/img/icon/arrow.svg" alt=""></div>
+
+    <!--==============================
+     Preloader
+  ==============================-->
+    <div class="preloader ">
+        <button class="th-btn preloaderCls">Cancel Preloader </button>
+        <div class="preloader-inner">
+            <div class="loader">
+                <img src="assets/img/theme-img/shopping-loader.gif" alt="">
+            </div>
+        </div>
+    </div>
+
+
+
+    
+    
+    <!--==============================
+    Sidemenu
+============================== -->
+    <div class="sidemenu-wrapper sidemenu-cart ">
+        <div class="sidemenu-content">
+            <button class="closeButton sideMenuCls"><i class="far fa-times"></i></button>
+            <div class="widget woocommerce widget_shopping_cart">
+                <h3 class="widget_title">Shopping cart</h3>
+                <div class="widget_shopping_cart_content">
+                    <ul class="woocommerce-mini-cart cart_list product_list_widget ">
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_6_1.png" alt="Cart Image">Fresh Vegetables Deals</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>39.00</span>
+                            </span>
+                        </li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_6_2.png" alt="Cart Image">Daily Fresh Organic Butter</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>38.00</span>
+                            </span>
+                        </li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_6_3.png" alt="Cart Image">New Hot Fruits Juice</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>37.00</span>
+                            </span>
+                        </li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_6_4.png" alt="Cart Image">Delicious Chips for Snacks</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>32.00</span>
+                            </span>
+                        </li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item">
+                            <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                            <a href="#"><img src="assets/img/product/product_6_5.png" alt="Cart Image">Strawberry Lucerne Lowfat</a>
+                            <span class="quantity">1 ×
+                                <span class="woocommerce-Price-amount amount">
+                                    <span class="woocommerce-Price-currencySymbol">$</span>29.00</span>
+                            </span>
+                        </li>
+                    </ul>
+                    <p class="woocommerce-mini-cart__total total">
+                        <strong>Subtotal:</strong>
+                        <span class="woocommerce-Price-amount amount">
+                            <span class="woocommerce-Price-currencySymbol">$</span>137.00</span>
+                    </p>
+                    <p class="woocommerce-mini-cart__buttons buttons">
+                        <a href="cart.php" class="th-btn wc-forward">View cart</a>
+                        <a href="checkout.php" class="th-btn checkout wc-forward">Checkout</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="popup-search-box d-none d-lg-block">
+        <button class="searchClose"><i class="fal fa-times"></i></button>
+        <form action="#">
+            <input type="text" placeholder="What are you looking for?">
+            <button type="submit"><i class="fal fa-search"></i></button>
+        </form>
+    </div>
+    
+    <!--==============================
+    Mobile Menu
+  ============================== -->
+    <div class="th-menu-wrapper">
+        <div class="th-menu-area text-center">
+            <button class="th-menu-toggle"><i class="fal fa-times"></i></button>
+            <div class="mobile-logo">
+                <a href="index.php"><img src="assets/img/logo.svg" alt="Erna"></a>
+            </div>
+            <div class="th-mobile-menu">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="shop-grid.php">Shop</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--============================== Header Area ==============================-->
+    <header class="th-header header-layout2">
+        <div class="header-top" data-bg-src="assets/img/shape/header-pattern.png">
+            <div class="container th-container">
+                <div class="row justify-content-center justify-content-md-between align-items-center gy-2">
+                    <div class="col-lg-4">
+                        <div class="header-links">
+                            <ul>
+                                <li><img src="assets/img/icon/phone2.svg" alt=""><a href="tel:+00123456789">+00 123 456
+                                        789</a></li>
+                                <li><i class="fa-solid fa-location-dot"></i>123 HR Street Line, UK</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="swiper th-slider" id="topSlide1" data-slider-options='{"effect":"slide"}'>
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                    <div class="marquee-card">
+                                        <a target="_blank" href="#">
+                                            <img src="assets/img/icon/fire.svg" alt="">Free Shipping On Order $180+</a>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="marquee-card">
+                                        <a target="_blank" href="#">
+                                            <img src="assets/img/icon/fire.svg" alt="">Deal of the day 35% off</a>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="marquee-card">
+                                        <a target="_blank" href="#">
+                                            <img src="assets/img/icon/fire.svg" alt="">Free
+                                            Today’s Deal $18+</a>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="marquee-card">
+                                        <a target="_blank" href="#">
+                                            <img src="assets/img/icon/fire.svg" alt="">$5 off your first order</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="menu-top">
+            <div class="container th-container">
+                <div class="row justify-content-center justify-content-lg-between align-items-center gy-2">
+                    <div class="col-auto">
+                        <div class="header-logo d-none d-lg-block">
+                            <a href="index.php"><img src="assets/img/logo-orange.svg" alt="Erna"></a>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <div class="header2-wrapp">
+                            <div class="header-info-wrap">
+                                <div class="header-info">
+                                    <div class="dropdown">
+                                        <div class="icon-btn">
+                                            <img src="assets/img/icon/bag2.svg" alt=""><span class="badge">2</span>
+                                        </div>
+                                        <div><span class="header-info_label">Your Cart</span>
+                                            <a href="#" class="header-info_link">$00.00</a>
+                                            <div class="cart-dropdown-menu">
+                                                <div class="widget woocommerce widget_shopping_cart style2">
+                                                    <div class="widget_shopping_cart_content">
+                                                        <ul class="woocommerce-mini-cart cart_list product_list_widget ">
+                                                            <li class="woocommerce-mini-cart-item mini_cart_item">
+                                                                <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                                                                <a href="#"><img src="assets/img/product/product_2_8.png" alt="Cart Image">Smart TV</a>
+                                                                <span class="quantity">1 ×
+                                                                    <span class="woocommerce-Price-amount amount">
+                                                                        <span class="woocommerce-Price-currencySymbol">$</span>990.00 USD</span>
+                                                                </span>
+                                                            </li>
+                                                            <li class="woocommerce-mini-cart-item mini_cart_item">
+                                                                <a href="#" class="remove remove_from_cart_button"><i class="far fa-times"></i></a>
+                                                                <a href="#"><img src="assets/img/product/product_2_7.png" alt="Cart Image">Camera</a>
+                                                                <span class="quantity">1 ×
+                                                                    <span class="woocommerce-Price-amount amount">
+                                                                        <span class="woocommerce-Price-currencySymbol">$</span> 690.00 USD</span>
+                                                                </span>
+                                                            </li>
+                                                        </ul>
+                                                        <p class="woocommerce-mini-cart__total total">
+                                                            <strong>Total:</strong>
+                                                            <span class="woocommerce-Price-amount amount">
+                                                                <span class="woocommerce-Price-currencySymbol">$</span>1680.00 USD</span>
+                                                        </p>
+                                                        <p class="woocommerce-mini-cart__buttons buttons">
+                                                            <a href="cart.php" class="th-btn wc-forward">View cart</a>
+                                                            <a href="checkout.php" class="th-btn checkout wc-forward">Checkout</a>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="sticky-wrapper">
+            <!-- Main Menu Area -->
+            <div class="menu-area">
+                <div class="container th-container">
+                    <div class="row align-items-center justify-content-between">
+                       
+                        <div class="col-auto me-xl-auto">
+                            <nav class="main-menu d-none d-lg-inline-block">
+                                <ul>
+                                    <li class="menu-item-has-children">
+                                        <a href="index.php">Home</a>
+                                    </li>
+                                    <li><a href="shop.php">Shop</a></li>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-auto">
+                            <div class="call-btn d-none d-lg-inline-flex">
+                                <div class="box-icon"><img src="assets/img/icon/truck2.svg" alt=""></div>
+                                <div class="media-body">
+                                    <span class="box-label">Free Shipping</span>
+                                    <h3 class="box-link mb-0">Over Order $280</h3>
+                                </div>
+                            </div>
+                            <button type="button" class="th-menu-toggle d-block d-lg-none"><i class="far fa-bars"></i></button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
